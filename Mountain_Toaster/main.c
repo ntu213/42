d5aa795673ee332746e11a9ed51b805663481350
Valentin Giraudon <vgiraudo@c1r3p3.42perpignan.fr>
2023-02-23 11:13:17 +0100

    edit

---
+++ Mountain_Toaster/main.c
@@ -0,0 +1,20 @@
+#include "toaster.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		ft_all();
+		return (0);
+	}
+}
+
+void	ft_all()
+{
+	t_testers strct = testers_default;
+
+	while (strct)
+	{
+		
+	}
+}
---
+++ Mountain_Toaster/toaster.h
@@ -0,0 +1,6 @@
+struct s_testers
+{
+	t
+} testers_default = {};
+
+typedef struct s_testers t_testers;
