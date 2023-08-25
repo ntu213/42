e061fc63c653a9de9b4d636124427bbd7167f4f9
Moi - Portable <nttu213@gmail.com>
2023-08-26 00:07:48 +0200

    edit

--- Transcendence/tsx_learn/OC/index.js
+++ Transcendence/tsx_learn/OC/index.js
@@ -1,9 +1,9 @@
-let pmp = prompt("Tape ton mot: ")
+//let pmp = prompt("Tape ton mot: ")
 let test = document.getElementById("test")
 let body = document.querySelector("body")
-if (pmp == "toto")
+//if (pmp == "toto")
 	console.log("wah tu sais ecrire")
-else
+//else
 	console.log("gros naze")
 console.log(test)
 setTimeout(function() {
@@ -11,3 +11,17 @@ setTimeout(function() {
 	console.log(test)
 }, 2000)
 console.log(body)
+
+button = document.getElementById("button1")
+button.addEventListener("click", function() {
+	let cache = button.textContent
+	button.textContent = "Hey! I'm a button clicked less than 1 sec ago!"
+	setTimeout(function(){
+		button.textContent = cache
+	}, 1000)
+})
+
+document.addEventListener("mousemove", function(event) {
+	coords = this.getElementById("coord")
+	coords.textContent = "X = " + event.clientX + "\nY = " + event.clientY
+})
